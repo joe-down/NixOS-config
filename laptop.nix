4e00e10d5a396d8fcb0ffb9f2159ff32f3cb1de5
Joe <30624504+joe-down@users.noreply.github.com>
2024-05-14 14:36:21 +0100

    autofreq

--- laptop.nix
+++ laptop.nix
@@ -9,6 +9,20 @@
     ];
     
   boot.initrd.luks.devices."luks-f14f0478-5671-497b-9d7f-cbab653ee2ac".device = "/dev/disk/by-uuid/f14f0478-5671-497b-9d7f-cbab653ee2ac";
+  
   networking.hostName = "joe-laptop";
-
+  
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "auto";
+      };
+      charger = {
+       governor = "performance";
+       turbo = "auto";
+      };
+    };
+  };
 }
