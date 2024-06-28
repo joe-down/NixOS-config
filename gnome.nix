8f741932ecc30986117633e63ed899b41cb5b552
Joe <30624504+joe-down@users.noreply.github.com>
2024-06-28 14:23:46 +0100

    gnome

--- gnome.nix
+++ gnome.nix
@@ -4,11 +4,15 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  programs.evince.enable = true;
 
   home-manager.users.joe = { pkgs, ... }: {
     home.packages = with pkgs ; [
                       gnome-latex
                       gnome.gnome-software
+                      gnome.gnome-tweaks
+                      plots
+                      gnomeExtensions.bluetooth-smart-lock
                     ];
   
     services.kdeconnect = {
