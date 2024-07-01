d960b97228ca9fd88602ad5131c470f322719c36
Joe <30624504+joe-down@users.noreply.github.com>
2024-07-01 18:27:55 +0100

    home manager user package config fix

--- common.nix
+++ common.nix
@@ -106,6 +106,7 @@
   };
 
   home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
   home-manager.users.joe = { pkgs, ... }: {
     home.packages = with pkgs ; [
                       spotify
