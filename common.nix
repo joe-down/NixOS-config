17223475b1e1f3f24d9c8a050b991f4937af7065
Joe <30624504+joe-down@users.noreply.github.com>
2024-07-25 14:47:26 +0100

    add configuration.nix template

--- common.nix
+++ common.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 {
@@ -228,10 +224,6 @@
       enable = true;
       extraPackages = tpkgs: { inherit (tpkgs) scheme-full; };
     };
-    
-    # The state version is required and should stay at the version you
-    # originally installed.
-    home.stateVersion = "23.11";
   };
 
   # List packages installed in system profile. To search, run:
@@ -257,14 +249,6 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
 
   services.avahi = {
     enable = true;
