c0f2229c43ba66d8f3708b191a7e1a7e625df113
Joe <30624504+joe-down@users.noreply.github.com>
2024-07-26 03:51:38 +0100

    btrfs laptop

--- laptop.nix
+++ laptop.nix
@@ -8,9 +8,6 @@
     ./common.nix
     ./gnome.nix
   ];
-
-  boot.initrd.luks.devices."luks-f14f0478-5671-497b-9d7f-cbab653ee2ac".device =
-    "/dev/disk/by-uuid/f14f0478-5671-497b-9d7f-cbab653ee2ac";
-
+  swapDevices = [{ device = "/swap/swapfile"; }];
   networking.hostName = "joe-laptop";
 }
