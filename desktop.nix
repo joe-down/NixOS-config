dad2a6c219a50a3ecea8e4b11cdd58cef2e2913e
Joe <30624504+joe-down@users.noreply.github.com>
2024-08-03 21:16:07 +0100

    tidy

--- desktop.nix
+++ desktop.nix
@@ -1,66 +1,62 @@
 {
   imports = [ ./common.nix ./kde.nix ./amd_cpu.nix ./nvidia.nix ];
 
-  networking.hostName = "joe-desktop";
-
   fileSystems = {
     "/".options = [ "compress=zstd" "discard=async" ];
     "/mnt/ssd".options = [ "compress=zstd" "discard=async" ];
     "/mnt/hdd".options = [ "compress=zstd" ];
   };
+  swapDevices = [{ device = "/swap/swapfile"; }];
+
+  networking.hostName = "joe-desktop";
 
   hardware.xone.enable = true;
 
-  swapDevices = [{ device = "/swap/swapfile"; }];
+  programs.dconf.enable = true;
 
-  services.pipewire.extraConfig.pipewire = {
-    "10-clock-rate" = {
-      "context.properties" = { "default.clock.rate" = 192000; };
+  services = {
+    pipewire.extraConfig.pipewire = {
+      "10-clock-rate" = {
+        "context.properties" = { "default.clock.rate" = 192000; };
+      };
+    };
+    hardware.openrgb = {
+      enable = true;
+      motherboard = "amd";
+    };
+    samba = {
+      enable = true;
+      openFirewall = true;
+      shares = {
+        Audiobooks = { path = "/mnt/hdd/Audiobooks/"; };
+        Books = { path = "/mnt/hdd/Books/"; };
+        Comics = { path = "/mnt/hdd/Comics/"; };
+        Music = { path = "/mnt/hdd/Music/Music/"; };
+        ROMs = { path = "/mnt/hdd/ROMs/ROMs/"; };
+      };
     };
   };
 
-  services.hardware.openrgb = {
-    enable = true;
-    motherboard = "amd";
-  };
-
-  programs.dconf.enable = true;
-
   home-manager.users.joe = { pkgs, ... }: {
-    xdg.configFile."autostart/spotify.desktop".source =
-      ./config/autostart/spotify.desktop;
-    xdg.configFile."autostart/discord.desktop".source =
-      ./config/autostart/discord.desktop;
-    xdg.configFile."autostart/steam.desktop".source =
-      ./config/autostart/steam.desktop;
-    xdg.configFile."autostart/openrgb.desktop".source =
-      ./config/autostart/openrgb.desktop;
-    programs.bash = {
-      shellAliases = {
+    xdg.configFile = {
+      "autostart/spotify.desktop".source = ./config/autostart/spotify.desktop;
+      "autostart/discord.desktop".source = ./config/autostart/discord.desktop;
+      "autostart/steam.desktop".source = ./config/autostart/steam.desktop;
+      "autostart/openrgb.desktop".source = ./config/autostart/openrgb.desktop;
+    };
+    programs = {
+      bash.shellAliases = {
         "joe-import-roms" =
           "igir move zip test clean --dat /mnt/hdd/ROMs/DATs/ --input /mnt/hdd/ROMs/ROMs/ --input ./ --output /mnt/hdd/ROMs/ROMs/ --dir-dat-name";
       };
-    };
-
-    programs.beets = {
-      enable = true;
-      settings = {
-        directory = "/mnt/hdd/Music/Music/";
-        library = "/mnt/hdd/Music/Beets/musiclibrary.db";
-        plugins = "chroma fetchart mbsync web";
+      beets = {
+        enable = true;
+        settings = {
+          directory = "/mnt/hdd/Music/Music/";
+          library = "/mnt/hdd/Music/Beets/musiclibrary.db";
+          plugins = "chroma fetchart mbsync web";
+        };
       };
-    };
-  };
-
-  services.samba = {
-    enable = true;
-    openFirewall = true;
-    shares = {
-      Audiobooks = { path = "/mnt/hdd/Audiobooks/"; };
-      Books = { path = "/mnt/hdd/Books/"; };
-      Comics = { path = "/mnt/hdd/Comics/"; };
-      Music = { path = "/mnt/hdd/Music/Music/"; };
-      ROMs = { path = "/mnt/hdd/ROMs/ROMs/"; };
     };
   };
 }
