9e13232ad93fff2aee3231150efc85dd3b735bbb
System administrator <30624504+joe-down@users.noreply.github.com>
2024-12-11 16:30:30 +0000

    gnome for nixos 24.11

--- gnome/gnome.nix
+++ gnome/gnome.nix
@@ -1,15 +1,16 @@
-{
+{ pkgs, ... }: {
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   programs.evince.enable = true;
-  programs.kdeconnect.package = gnomeExtensions.gsconnect;
-  programs.firefox.nativeMessagingHosts.gsconnect = true;
+  programs.kdeconnect.package = pkgs.gnomeExtensions.gsconnect;
+  programs.firefox.nativeMessagingHosts.packages =
+    [ pkgs.gnomeExtensions.gsconnect ];
 
   home-manager.users.joe = { pkgs, ... }: {
     home.packages = with pkgs; [
-      gnome-latex
-      gnome.gnome-software
-      gnome.gnome-tweaks
+      enter-tex
+      gnome-software
+      gnome-tweaks
       plots
       gnomeExtensions.appindicator
     ];
